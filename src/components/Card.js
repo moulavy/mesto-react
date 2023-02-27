9994704059a65cfe485a70cb1485dfdbe07f4291
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-02-27 18:29:51 +0300

    вывод карточек

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Card(props) {
+   return (
+      <li class="elements__element" key={props.card._id}>
+         <button class="elements__button-delete "></button>
+         <img class="elements__image" src={props.card.link} />
+         <h2 class="elements__title">{props.card.name}</h2>
+         <div class="elements__button-like">
+            <button aria-label="Like" class="elements__button-heart"></button>
+            <p class="elements__like-counter">{props.card.likes.length}</p>
+         </div>
+      </li>
+   )
+}
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
-import api from '../utils/api.js'
+import api from '../utils/api.js';
+import Card from './Card.js'
 function Main(props) {   
    const [userName, setUserName] = React.useState('');
    const [userDescription, setUserDescription] = React.useState('');
    const [userAvatar, setUserAvatar] = React.useState('');
+   const [cards, setCards] = React.useState([]);
 
    useEffect(() => {
       Promise.all([api.getUserInfo(), api.getInitialCards()])
@@ -11,6 +13,7 @@ function Main(props) {
             setUserName(resUser.name);
             setUserDescription(resUser.about);
             setUserAvatar(resUser.avatar);
+            setCards(resCards);
          })
          .catch((err) => {
             console.log(err);
@@ -35,17 +38,11 @@ function Main(props) {
    </section>
    <section className="elements">
       <ul className="elements__list">
-         <template id="elements__element">
-            <li className="elements__element">
-               <button className="elements__button-delete "></button>
-               <img className="elements__image" />
-               <h2 className="elements__title"></h2>
-               <div className="elements__button-like">
-                  <button aria-label="Like" className="elements__button-heart"></button>
-                  <p className="elements__like-counter">8</p>
-               </div>
-            </li>
-         </template>
+               {cards.map((card) =>
+                  <Card
+                     card={card}
+                  />
+               )}
       </ul>
    </section>
       </main>
